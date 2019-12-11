1d00e0c2e232b0405ee53507741870ddd34b6064
Sai Sandeep <nukala.sandy@gmail.com>
2019-12-11 23:19:13 +0530

    Initial Commit

---
+++ SSChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSChat
+//
+//  Created by Sai Sandeep on 11/12/19.
+//  Copyright © 2019 iOS Revisited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
